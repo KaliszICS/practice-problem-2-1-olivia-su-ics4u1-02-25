313d0dcc2f7548731bcb601ee594638989a1480b
olivia-su-ics4u1-02-25 <olivia.su@student.tdsb.on.ca>
2025-03-30 23:34:29 +0000

    commit

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -4,6 +4,43 @@ public class PracticeProblem {
 
 	}
 
-	
+	public static int find (int[] array, int number){
+		int index = -1;
 
+		for (int i = 0; i < array.length; i++){
+			if (number == array[i]){
+				index = i;
+				i = array.length;
+			}
+		}
+		return index;
+	}
+
+	public static int findLast(String[] array, String string){
+		int index = -1;
+
+		for (int i = 0; i < array.length; i++){
+			if (string == array[i]){
+				index = i;
+			}
+		}
+		return index;
+	}
+
+	public static int findSecond(char[] array, char character){
+		int index = -1;
+		int counter = 0;
+
+		for (int i = 0; i < array.length; i++){
+			if (character == array[i]){
+				counter += 1; 
+				index = i;
+				
+				if (counter == 2){
+					i = array.length;
+				}
+			}
+		}
+		return index;
+	}
 }
